8b01f4cc3eff5c8497eecdd3eaf5544fe7b82886
neeels27 <75979083+neeels27@users.noreply.github.com>
2023-04-06 15:59:46 +0200

    Add files via upload

---
+++ src/api/piste/controllers/piste.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * piste controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::piste.piste');
---
+++ src/api/piste/routes/piste.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * piste router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::piste.piste');
